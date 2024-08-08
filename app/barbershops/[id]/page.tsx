432cb8de6048e5505e99b5346ec5f7dabe6ad4f7
Lucas Hackbart Döhnert <lucashackd@gmail.com>
2024-08-08 00:31:48 -0300

    feat: add PhoneItem component and Footer to layout

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,3 +1,4 @@
+import PhoneItem from "@/app/_components/phone-item"
 import ServiceItem from "@/app/_components/service-item"
 import { Button } from "@/app/_components/ui/button"
 import { db } from "@/app/_lib/prisma"
@@ -13,7 +14,6 @@ interface BarbershopPageProps {
 }
 
 const BarbershoPage = async ({ params }: BarbershopPageProps) => {
-  //get in database
   const barbershop = await db.barbershop.findUnique({
     where: {
       id: params.id,
@@ -83,13 +83,20 @@ const BarbershoPage = async ({ params }: BarbershopPageProps) => {
       </div>
 
       {/* SERVIÇOS */}
-      <div className="space-y-3 p-5">
+      <div className="space-y-3 border-b border-solid p-5">
         <h2 className="text-xs font-bold uppercase text-gray-400">Serviços</h2>
         <div className="space-y-3">
           {barbershop.services.map((service) => (
             <ServiceItem key={service.id} service={service} />
           ))}
         </div>
+      </div>
+
+      {/* CONTATOS */}
+      <div className="space-y-3 p-5">
+        {barbershop.phones.map((phone) => (
+          <PhoneItem key={phone} phone={phone} />
+        ))}
       </div>
     </div>
   )
